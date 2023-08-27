a6e2ebe865e29a05b32598dbf4f1de10e69dc23b
Hamza Baba <hamzabande@outlook.com>
2023-08-27 10:35:45 +0100

    deployment

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,18 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
-app.use(express.json());
-app.use(cookieParser());
+
 app.use(cors({
     origin: "http://localhost:3000",
     credentials: true,
 }));
-app.use("/", express.static("uploads"));
-app.use("/", (req,res) => {
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/", express.static(path.join(__dirname,"./uploads")));
+app.use("/test", (req,res) => {
     res.send("Hello world");
 });
 app.use(bodyParser.urlencoded({extended:true, limit:"50mb"}));
